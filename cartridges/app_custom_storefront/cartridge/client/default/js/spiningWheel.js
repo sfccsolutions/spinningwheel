c146eee55cb761f2a0d09fa37837d3b7e024dcc2
Tayyaba Naseem Ashraf <53142748+taibaashraf@users.noreply.github.com>
2022-09-28 15:20:09 +0500

    Updated Version

--- cartridges/app_custom_storefront/cartridge/client/default/js/spiningWheel.js
+++ cartridges/app_custom_storefront/cartridge/client/default/js/spiningWheel.js
@@ -10,20 +10,22 @@ var padding = { top: 20, right: 40, bottom: 0, left: 0 },
 //randomNumbers = getRandomNumbers();
 //http://osric.com/bingo-card-generator/?title=HTML+and+CSS+BINGO!&words=padding%2Cfont-family%2Ccolor%2Cfont-weight%2Cfont-size%2Cbackground-color%2Cnesting%2Cbottom%2Csans-serif%2Cperiod%2Cpound+sign%2C%EF%B9%A4body%EF%B9%A5%2C%EF%B9%A4ul%EF%B9%A5%2C%EF%B9%A4h1%EF%B9%A5%2Cmargin%2C%3C++%3E%2C{+}%2C%EF%B9%A4p%EF%B9%A5%2C%EF%B9%A4!DOCTYPE+html%EF%B9%A5%2C%EF%B9%A4head%EF%B9%A5%2Ccolon%2C%EF%B9%A4style%EF%B9%A5%2C.html%2CHTML%2CCSS%2CJavaScript%2Cborder&freespace=true&freespaceValue=Web+Design+Master&freespaceRandom=false&width=5&height=5&number=35#results
 var data = [
-    { "label": "Dell LAPTOP", "value": 1, "question": "You Won Dell Laptop....Congratulations!!" },
-    { "label": "IMAC PRO", "value": 2, "question": "You Won IMAC PRO....Congratulations!!" },
-    { "label": "FREE VOUCHER", "value": 3, "question": "You Won Free Voucher....Congratulations!!" },
-    { "label": "BONUS POINTS", "value": 4, "question": "You Won Bonus Points....Congratulations!!" },
-    { "label": "FERRARI", "value": 5, "question": "You Won FERRARI....Congratulations!!" },
-    { "label": "APARTMENT", "value": 6, "question": "You Won APARTMENT....Congratulations!!" },
-    { "label": "FREE SHIPPING", "value": 7, "question": "You Won Free Shipping on your delievery....Congratulations!!" }, //nesting
-    { "label": "JEWELLERY", "value": 8, "question": "You Won pair of Earrings....Congratulations!!" },
+    { "label": "Dell LAPTOP", "value": 1, "question": "You Won Dell Laptop..Congratulations!" },
+    { "label": "IMAC PRO", "value": 2, "question": "You Won IMAC PRO..Congratulations!" },
+    { "label": "FREE VOUCHER", "value": 3, "question": "You Won Free Voucher..Congratulations!" },
+    { "label": "BONUS POINTS", "value": 4, "question": "You Won Bonus Points..Congratulations!" },
+    { "label": "FERRARI", "value": 5, "question": "You Won FERRARI..Congratulations!" },
+    { "label": "APARTMENT", "value": 6, "question": "You Won APARTMENT..Congratulations!" },
+    { "label": "FREE SHIPPING", "value": 7, "question": "You Won Free Shipping..Congratulations!" }, //nesting
+    { "label": "JEWELLERY", "value": 8, "question": "You Won pair of Earrings..Congratulations!" },
 ];
 var svg = d3.select('#chart')
     .append("svg")
     .data([data])
     .attr("width", w + padding.left + padding.right)
-    .attr("height", h + padding.top + padding.bottom);
+    .attr("height", h + padding.top + padding.bottom)
+    .style({ "margin-left": "-13%" });
+
 var container = svg.append("g")
     .attr("class", "chartholder")
     .attr("transform", "translate(" + (w / 2 + padding.left) + "," + (h / 2 + padding.top) + ")");
